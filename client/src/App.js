84e686b427e3af3b810cad5549fddd8db930a6c8
greemaltend4 <dengestabiy@hotmail.com>
2025-09-30 14:45:28 +0000

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bienvenue dans l'application de gestion e-commerce!</h1>
+    </div>
+  );
+};
+
+export default App;
